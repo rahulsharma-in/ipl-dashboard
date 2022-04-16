f883210e9755febc6d757fbcae736719a3f810c9
Rahul Sharma <rahulsharma250198@gmail.com>
2022-04-16 19:05:27 +0530

    Making Team a JPA Entity, added NoArgsConstructor and constructor with teamName and totalMatches as arguemnt

--- src/main/java/com/ipl/ipldashboard/model/Team.java
+++ src/main/java/com/ipl/ipldashboard/model/Team.java
@@ -1,15 +1,29 @@
 package com.ipl.ipldashboard.model;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Getter
 @Setter
+@Entity
+@NoArgsConstructor
 public class Team {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long Id;
     private String teamName;
     private int totalMatches;
     private int totalWins;
 
+    public Team(String teamName, int totalMatches) {
+        this.teamName = teamName;
+        this.totalMatches = totalMatches;
+    }
 }
