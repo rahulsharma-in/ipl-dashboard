3c67e05c12ad21f50e7786238c9718792e7c3a8a
Rahul Sharma <rahulsharma250198@gmail.com>
2022-04-16 22:46:54 +0530

    findByTeamName API update with team matches field filled

--- src/main/java/com/ipl/ipldashboard/services/impl/TeamServiceImpl.java
+++ src/main/java/com/ipl/ipldashboard/services/impl/TeamServiceImpl.java
@@ -1,25 +1,36 @@
 package com.ipl.ipldashboard.services.impl;
 
 import com.ipl.ipldashboard.Repository.TeamRepository;
+import com.ipl.ipldashboard.model.Match;
 import com.ipl.ipldashboard.model.Team;
+import com.ipl.ipldashboard.services.MatchService;
 import com.ipl.ipldashboard.services.TeamService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class TeamServiceImpl implements TeamService {
 
     private TeamRepository teamRepository;
+    private MatchService matchService;
 
-    public TeamServiceImpl(TeamRepository teamRepository) {
+    public TeamServiceImpl(TeamRepository teamRepository,
+                           MatchService matchService) {
         this.teamRepository = teamRepository;
+        this.matchService = matchService;
     }
 
     @Override
     public Team findByTeamName(String teamName) {
-        Optional<Team> team = teamRepository.findByTeamName(teamName);
-        if (team.isPresent()) return team.get();
+        Optional<Team> optionalTeam = teamRepository.findByTeamName(teamName);
+        if (optionalTeam.isPresent()) {
+            Team team = optionalTeam.get();
+            List<Match> matches = matchService.findByTeam1OrTeam2(teamName, teamName, 0, 5);
+            team.setMatches(matches);
+            return team;
+        }
         throw new RuntimeException("Invalid Team Name found");
     }
 
