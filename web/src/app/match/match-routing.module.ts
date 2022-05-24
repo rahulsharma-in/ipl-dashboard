bffe8f44496a23118291b736b5c4625cc5b88449
Rahul Sharma <rahulsharma250198@gmail.com>
2022-05-25 01:36:11 +0530

    route for matches of a team for a particular year

--- web/src/app/match/match-routing.module.ts
+++ web/src/app/match/match-routing.module.ts
@@ -1,8 +1,11 @@
+import { MatchLandingComponent } from './match-landing/match-landing.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : ':teamName/:year', component : MatchLandingComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
