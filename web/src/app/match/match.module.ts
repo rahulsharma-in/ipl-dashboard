cc7927dbf5876b52be272014924294fe7f460788
Rahul Sharma <rahulsharma250198@gmail.com>
2022-05-25 01:37:26 +0530

    Added MatchLandingComponent in match module declarations

--- web/src/app/match/match.module.ts
+++ web/src/app/match/match.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatchRoutingModule } from './match-routing.module';
+import { MatchLandingComponent } from './match-landing/match-landing.component';
 
 
 @NgModule({
-  declarations: [MatchDetailCardComponent, MatchSmallCardComponent],
+  declarations: [MatchDetailCardComponent, MatchSmallCardComponent, MatchLandingComponent],
   imports: [
     CommonModule,
     MatchRoutingModule
