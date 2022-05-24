cf5c24c7afc46688b62c55902cfa3df6755178df
Rahul Sharma <rahulsharma250198@gmail.com>
2022-05-25 00:24:03 +0530

    finding opposite team, resultMargin, winBy (runs or wickets)

--- web/src/app/match/match-small-card/match-small-card.component.ts
+++ web/src/app/match/match-small-card/match-small-card.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class MatchSmallCardComponent implements OnInit {
   @Input() match: any;
+  @Input() teamName: string;
+  public otherTeam: string;
+  public isDraw: boolean;
+  public winBy: string;
+  public resultMargin: string;
   constructor() { }
 
   ngOnInit(): void {
+    this.otherTeam = this.match.team1 === this.teamName ? this.match.team2 : this.match.team1;
+    this.winBy = this.match.winByRuns === "0" ? "wickets" : "runs";
+    this.resultMargin = this.match.winByRuns === "0" ? this.match.winByWickets : this.match.winByRuns;
   }
 
 }
