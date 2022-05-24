7b3de926f97dd690dfdef214292c13a8306e1a0e
Rahul Sharma <rahulsharma250198@gmail.com>
2022-05-24 23:21:19 +0530

    Allowing CrossOrigin for TeamController

--- src/main/java/com/ipl/ipldashboard/controller/TeamController.java
+++ src/main/java/com/ipl/ipldashboard/controller/TeamController.java
@@ -2,11 +2,13 @@ package com.ipl.ipldashboard.controller;
 
 import com.ipl.ipldashboard.model.Team;
 import com.ipl.ipldashboard.services.TeamService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamService teamService;
