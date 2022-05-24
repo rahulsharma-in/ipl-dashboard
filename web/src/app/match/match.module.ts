ea0e3c27724e57b05a674dfd86c9a209f9dee891
Rahul Sharma <rahulsharma250198@gmail.com>
2022-05-24 23:28:36 +0530

    match-detail-card and match-small-card declaration/export in match module

--- web/src/app/match/match.module.ts
+++ web/src/app/match/match.module.ts
@@ -1,15 +1,17 @@
+import { MatchSmallCardComponent } from './match-small-card/match-small-card.component';
+import { MatchDetailCardComponent } from './match-detail-card/match-detail-card.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MatchRoutingModule } from './match-routing.module';
-import { MatchComponent } from './match/match.component';
 
 
 @NgModule({
-  declarations: [MatchComponent],
+  declarations: [MatchDetailCardComponent, MatchSmallCardComponent],
   imports: [
     CommonModule,
     MatchRoutingModule
-  ]
+  ],
+  exports: [MatchDetailCardComponent, MatchSmallCardComponent]
 })
 export class MatchModule { }
