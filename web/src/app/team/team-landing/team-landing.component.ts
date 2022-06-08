3eda64df8c49acb557e0f350339e0e589f1c93b5
Rahul Sharma <rahulsharma250198@gmail.com>
2022-06-09 02:30:41 +0530

    wins/losses in team-landing.component.html

--- web/src/app/team/team-landing/team-landing.component.ts
+++ web/src/app/team/team-landing/team-landing.component.ts
@@ -12,14 +12,18 @@ export class TeamLandingComponent implements OnInit {
   public team: any = [];
   public oneSlicedTeamMatches: any = [];
   private teamName: string;
+  public wins: number;
+  public losses: number;
   constructor(public matchService: MatchService, public route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.teamName = this.route.snapshot.paramMap.get('teamName')
+    this.teamName = this.route.snapshot.paramMap.get('teamName');
     this.matchService.fetchMatches(this.teamName).subscribe((response: any) => {
       this.team = response;
       if (this.team != undefined && this.team.matches.length > 1) {
         this.oneSlicedTeamMatches = this.team.matches.slice(1, this.team.matches.length);
+        this.wins = this.team.totalWins;
+        this.losses = this.team.totalMatches - this.wins;
       }
     })
   }
